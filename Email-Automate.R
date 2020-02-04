64b38aaed9b37ddc70f3d220e4c94a24ce8ce471
Swapnil S <50592783+swapnil-sarda@users.noreply.github.com>
2020-02-04 22:39:12 +0000

    Create Email-Automate.R

---
+++ Email-Automate.R
@@ -0,0 +1,66 @@
+library(gmailr)
+library(tidyverse)
+library(fs)
+library(lubridate)
+library(rmarkdown)
+library(lubridate)
+
+
+# Email Parameters ---
+to <- "swapnil.sarda@outlook.com"
+subject <- "Automated report"
+body <- str_glue("
+  Hello Swapnil,
+  This is an automated report containing visualisations."
+)
+
+
+# Save file location
+file_path <- now() %>%
+  str_replace_all("[[:punct]]","_")%>%
+  str_replace(" ","T")%>%
+  str_c("_Automated_Report.html")
+
+#Rendering the Rmarkdown
+render(
+  input = "Automate.Rmd",
+  output_format = "html_document",
+  output_file = file_path,
+  output_dir = "Home"
+  )
+
+#Gmail API automation
+
+#Storing the client id and secret key
+gm_auth_configure(path = "filename.json")
+
+#Account through which email needs to be sent.
+gm_auth(email = "email account")
+
+#Email details
+email <- gm_mime()%>%
+  gm_to("reciever")%>%
+  gm_from("email through which email needs to be sent")%>%
+  gm_subject(" ")%>%
+  gm_text_body(" ")%>%
+  gm_attach_file(str_c("Home/",file_path)) #File location
+
+
+#Single click report sending option.
+shinyApp( 
+  
+  ui <- fluidPage(
+    actionButton("Send","Send report"),
+    mainPanel(
+      includeHTML(str_c("Home/",file_path)
+    )
+  )),
+  
+  server <- function(input, output,session){
+    
+    observeEvent(input$Send, gm_send_message(email))
+  }
+  
+)
+
+
